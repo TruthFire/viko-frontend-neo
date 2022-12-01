ab4360460a6bfc4c945653b7035b824bcd4b6fc9
TruthFire <truth.fire@yandex.ru>
2022-12-01 23:58:02 +0200

    review from validation

--- viko-frontend-neo-tw/src/js/reviewValidation.js
+++ viko-frontend-neo-tw/src/js/reviewValidation.js
@@ -1,4 +1,5 @@
-document.addEventListener('', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  //   debugger;
   const form = document.getElementById('reviewForm');
   const username = document.getElementById('nameInput');
   const email = document.getElementById('emailInput');
@@ -11,11 +12,49 @@ document.addEventListener('', () => {
     validateReviewForm();
   });
 
+  rating.addEventListener('change', (e) => {
+    if (rating.value > 5) {
+      rating.value = 5;
+    }
+    if (rating.value < 0) {
+      rating.value = 0;
+    }
+    console.log(e);
+  });
+
+  const isEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
   const setError = (el, message) => {
-
+    // debugger;
     const inputControl = el.parentElement;
-    const errorSpan = parentElement.querySelector
+    const errorSpan = inputControl.querySelector('.error');
 
+    errorSpan.innerText = message;
+
+    var inp = inputControl.querySelector('input');
+    inp === null ? (inp = inputControl.querySelector('textarea')) : '';
+
+    inp.classList.add('ring');
+    inp.classList.add('ring-red-800');
+    inp.classList.remove('ring-green-800');
+  };
+
+  const setSucess = (el) => {
+    const inputControl = el.parentElement;
+    const errorSpan = inputControl.querySelector('.error');
+
+    errorSpan.innerText = '';
+
+    var inp = inputControl.querySelector('input');
+    inp === null ? (inp = inputControl.querySelector('textarea')) : '';
+
+    inp.classList.add('ring');
+    inp.classList.remove('ring-red-800');
+    inp.classList.add('ring-green-800');
   };
 
   const validateReviewForm = () => {
@@ -24,9 +63,40 @@ document.addEventListener('', () => {
     const orderDateVal = orderDate.value.trim();
     const ratingVal = rating.value.trim();
     const reviewTextVal = reviewText.value.trim();
-  };
 
-  if(usernameVal === '') {
-    setError()
-  }
+    if (usernameVal === '') {
+      setError(username, 'Name is required');
+    } else {
+      setSucess(username);
+    }
+    if (emailVal === '') {
+      setError(email, 'Email is required');
+    } else if (isEmail(email)) {
+      setError(email, 'Provide a valid email address');
+    } else {
+      setSucess(email);
+    }
+
+    if (isNaN(Date.parse(orderDateVal)) || orderDateVal === '') {
+      setError(orderDate, 'Date of order is required');
+    } else {
+      setSucess(orderDate);
+    }
+
+    if (ratingVal === '') {
+      setError(rating, "Rating value can't be empty");
+    } else if (ratingVal > 5 || ratingVal < 0) {
+      setError(
+        rating,
+        'Rating value should be bigger or equal to 0 and smaller or equal to 5'
+      );
+    } else {
+      setSucess(rating);
+    }
+    if (reviewTextVal === '') {
+      setError(reviewText, "Review text can't be empty");
+    } else {
+      setSucess(reviewText);
+    }
+  };
 });
